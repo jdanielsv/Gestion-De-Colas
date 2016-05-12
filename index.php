138835404c4fd5f23e44f21b97fb0d66d58668e6
David <davidsalascastro@gmail.com>
2016-05-12 18:26:35 +0200

    url relativas

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
                 <div class="row">
                     <?php require(dirname(__FILE__).'/view/menu.php'); ?>
                         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                            <?php //require(dirname(__FILE__) . '/view/usuario/usuarioMostrar.php'); ?>
-                            <?php require(dirname(__FILE__) . '/view/cola/colaMostrar.php'); ?>
+                            <?php require(dirname(__FILE__) . '/view/usuario/usuarioMostrar.php'); ?>
+                            <?php //require(dirname(__FILE__) . '/view/cola/colaMostrar.php'); ?>
                         </div>
                 </div>
     </body>
--- view/usuario/usuarioMostrar.php
+++ view/usuario/usuarioMostrar.php
@@ -1,54 +1,35 @@
-<<<<<<< Updated upstream
-<?php
+<?php require('./conf/conexion.php'); 
+$conn=conexion(); 
+if ($conn->connect_error) { die("Fallo en la conexión: " . $conn->connect_error); } else{ $sql="SELECT * FROM usuario"; $result = $conn->query($sql); } ?>
 
-require ('/conf/conexion.php');
+    <h1 class="page-header">Listado usuarios</h1>
 
-$conn=conexion();
+    <div class="row">
+        <div class="col-md-12">
 
-if ($conn->connect_error) {
-    die("Fallo en la conexión: " . $conn->connect_error);
-} else{
-    $sql="SELECT * FROM usuario";
-    $result = $conn->query($sql);
-}
-?>
-=======
-<h1 class="page-header">Listado usuarios</h1>
->>>>>>> Stashed changes
-<div class="row">
-    <div class="col-md-12">
-
-        <div class="panel panel-default panel-table">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col col-xs-6">
-<<<<<<< Updated upstream
-                        <h3 class="panel-title">Listado usuario</h3>
-=======
->>>>>>> Stashed changes
-                    </div>
-                    <div class="col col-xs-6 text-right">
-                        <button type="button" class="btn btn-sm btn-primary btn-create" data-toggle="modal" data-target="#myModal">Nuevo usuario</button>
+            <div class="panel panel-default panel-table">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col col-xs-6"></div>
+                        <div class="col col-xs-6 text-right">
+                            <button type="button" class="btn btn-sm btn-primary btn-create" data-toggle="modal" data-target="#myModal">Nuevo usuario</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="panel-body">
-                <table class="table table-striped table-bordered table-list">
-                    <thead>
-                        <tr>
-                            <th><em class="fa fa-cog"></em></th>
-                            <th class="hidden-xs">ID</th>
-                            <th>Nombre</th>
-                            <th>Apellidos</th>
-                            <th>Contraseña</th>
-                            <th>Rol</th>
-                            <th>Correo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-<<<<<<< Updated upstream
-                            
-                            
+                <div class="panel-body">
+                    <table class="table table-striped table-bordered table-list">
+                        <thead>
+                            <tr>
+                                <th><em class="fa fa-cog"></em></th>
+                                <th class="hidden-xs">ID</th>
+                                <th>Nombre</th>
+                                <th>Apellidos</th>
+                                <th>Contraseña</th>
+                                <th>Rol</th>
+                                <th>Correo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             <?php
                             while($row = $result->fetch_assoc()) {
     
@@ -64,65 +45,34 @@ if ($conn->connect_error) {
                             }
                             
                             ?>
-                </tbody>
-            </table>
+                        </tbody>
+                    </table>
 
-    </div>
-    <div class="panel-footer">
-        <div class="row">
-            <div class="col col-xs-4">Página 1 de 1
-            </div>
-            <div class="col col-xs-8">
-                <ul class="pagination hidden-xs pull-right">
-                    <li><a href="#">1</a></li>
-                </ul>
-                <ul class="pagination visible-xs pull-right">
-                    <li><a href="#">«</a></li>
-                    <li><a href="#">»</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-
-</div>
-=======
-                        <tr>
-                            <td align="center">
-                                <a class="btn btn-default"><em class="fa fa-pencil"></em></a>
-                                <a class="btn btn-danger"><em class="fa fa-trash"></em></a>
-                            </td>
-                            <td class="hidden-xs">1</td>
-                            <td>David</td>
-                            <td>Salas castro</td>
-                            <td>pass</td>
-                            <td>Admin</td>
-                            <td>davidsalascastro@gmail.com</td>
-                        </tr>
-                    </tbody>
-                </table>
-
-            </div>
-            <div class="panel-footer">
-                <div class="row">
-                    <div class="col col-xs-4">Página 1 de 1
-                    </div>
-                    <div class="col col-xs-8">
-                        <ul class="pagination hidden-xs pull-right">
-                            <li><a href="#">1</a></li>
-                        </ul>
-                        <ul class="pagination visible-xs pull-right">
-                            <li><a href="#">«</a></li>
-                            <li><a href="#">»</a></li>
-                        </ul>
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col col-xs-4">Página 1 de 1
+                        </div>
+                        <div class="col col-xs-8">
+                            <ul class="pagination hidden-xs pull-right">
+                                <li><a href="#">1</a></li>
+                            </ul>
+                            <ul class="pagination visible-xs pull-right">
+                                <li><a href="#">«</a></li>
+                                <li><a href="#">»</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
 
+        </tbody>
+        </table>
+
     </div>
->>>>>>> Stashed changes
-</div>
+    </div>
 
 
 
@@ -130,9 +80,9 @@ if ($conn->connect_error) {
 
 
 
-<!-- Ventana emergente -->
+    <!-- Ventana emergente -->
 
-<!--<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <!--<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 <div class="modal-dialog" role="document">
 <div class="modal-content">
 <div class="modal-header">
