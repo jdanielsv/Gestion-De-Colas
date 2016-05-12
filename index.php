443d2cfaca380083118ef17d3dbb15badeaeac90
David <davidsalascastro@gmail.com>
2016-05-12 18:34:15 +0200

    todo bin

--- index.php
+++ index.php
@@ -4,26 +4,8 @@
 
 <!-- head -->
 <?php require(dirname(__FILE__) . '/view/header.php'); ?>
-<<<<<<< HEAD
-
-<body cz-shortcut-listen="true">
-
-    <!-- menu navegacion superior -->
-    <?php require("/view/navegacionSuperior.php"); ?>
-    
-    <div class="container-fluid">
-        <div class="row">
-            
-            <!--menuuu izquierda-->
-            <?php require("/view/menu.php"); ?>
-            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                <h1 class="page-header">Panel principal</h1>
-
-                <!-- Aqui se muestra contenido que cambia -->
-=======
     <body cz-shortcut-listen="true">
         <?php require(dirname(__FILE__) . '/view/navegacionSuperior.php'); ?>
->>>>>>> origin/master
 
             <div class="container-fluid">
                 <div class="row">
